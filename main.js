2b24ab552a4bbe50d4464f1be3bb51f72796bee8
unknown <maximoalexisflorianmatos@gmail.com>
2024-06-19 23:04:56 -0400

    class added

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-let tijera = {
-  src: "pngwing.com.png",
-  alt: "imagen 1",
-  width: 50,
-  y: 0,
-  x: 12,
-};
+const caja = document.getElementById("Board");
+const boton = document.getElementById("boton");
+// const imgpiedra = document.getElementById("rock");
+
+
+
+// getting box's dimensions
+let boxTop = caja.offsetTop;
+let boxBottom = caja.offsetHeight / 2.3;
+let boxLeft = caja.offsetLeft;
+let boxRight = window.outerWidth - caja.offsetWidth;
+
+
+console.log(boxTop);
+console.log(boxBottom);
+console.log(boxLeft);
+console.log(boxRight);
 
 let piedra = {
   src: "rock.png",
@@ -12,86 +22,100 @@ let piedra = {
   direccionX: "derecha" || "izquierda",
   direccionY: "arriba" || "abajo",
   width: 50,
-  step: 20,
-  y: Math.floor(Math.random() * 100),
-  x: Math.floor(Math.random() * 100),
+  step: 5,
+  y: 0,
+  x: 0,
 };
 
-//
-const caja = document.getElementById("Board");
-const boton = document.getElementById("boton");
-const imgpiedra = document.getElementById("rock");
+class element {
+  src;
+  alt;
+  direccionX;
+  direccionY;
+  width = `50px`;
+  step = 5;
+  y=0;
+  x=0;
+  node;
 
-let boxTop = caja.offsetTop;
-let boxBottom = window.outerHeight - caja.offsetHeight ;
-let boxLeft = caja.offsetLeft;
-let boxRight = window.outerWidth - caja.offsetWidth;
+  constructor(src, alt) {
+    this.src = src;
+    this.alt = alt;
+    this.direccionX = 'derecha';
+    this.direccionY = 'abajo'
+    this.createDomElement() 
+  }
+  createDomElement() {
+    const element1 = document.createElement("img");
+    element1.style.top = "0px";
+    element1.style.bottom = "0px";
+    element1.style.right = "0px";
+    element1.style.left = "0px";
+    element1.style.width = this.width
+    element1.src = this.src
+    element1.alt = this.alt
+    element1.style.position = 'absolute'
+    return this.node = element1;
+  }
+  moveFoward() {
+    return (this.x += this.step);
+  }
+  // move on X backward
+  moveBackward() {
+    return (this.x -= this.step);
+  }
+  //move on Y up
+  moveUp() {
+    return (this.y += this.step);
+  }
 
-console.log(boxTop);
-console.log(boxBottom);
-console.log(boxLeft);
-console.log(boxRight);
-
-imgpiedra.style.top = `${0}px`;
-imgpiedra.style.bottom = `${0}px`;
-imgpiedra.style.right = "0px";
-imgpiedra.style.left = "0px";
-
-
-function moveBackward() {
-  imgpiedra.style.transform = `translateX(${piedra.x}px)`;
-  piedra.x -= piedra.step;
-}
-
-function moveFoward() {
-  imgpiedra.style.transform = `translateX(${piedra.x}px)`;
-  piedra.x += piedra.step;
-}
-
-function moveUp() {
-  imgpiedra.style.top = piedra.y;
-  piedra.y += piedra.step;
-}
-
-function moveDown() {
-  imgpiedra.style.top = piedra.y;
-  piedra.y -= piedra.step;
-}
-function getDirectionY() {
-  if (piedra.y > boxBottom) {
-    piedra.direccionY = "abajo";
-  } else if (piedra.y < -boxBottom) {
-    piedra.direccionY = "arriba";
+  moveDown() {
+    return (this.y -= this.step);
+  }
+  //Change the y's direction
+  getDirectionY() {
+    if (this.y > boxBottom) {
+      this.direccionY = "abajo";
+    } else if (this.y < -boxBottom) {
+      this.direccionY = "arriba";
+    }
+  }
+  //Change the x's direction
+  getDirectionX() {
+    if (this.x > boxRight) {
+      this.direccionX = "izquierda";
+    } else if (this.x < -boxRight) {
+      this.direccionX = "derecha";
+    }
+  }
+  movement() {
+    this.getDirectionX();
+    this.getDirectionY();
+    if (this.direccionX === "derecha" && this.direccionY === "arriba") {
+      this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveDown()}px)`;
+    } else if (
+      this.direccionX === "izquierda" &&
+      this.direccionY === "arriba"
+    ) {
+      this.node.style.transform = `translate(${ this.moveBackward()}px,${this.moveDown()}px)`;
+    } else if (this.direccionX === "derecha" && this.direccionY === "abajo") {
+      this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveUp()}px)`;
+    } else if (
+      this.direccionX === "izquierda" &&
+      this.direccionY === "abajo"
+    ) {
+      this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveUp()}px )`;
+    }
   }
 }
 
-function getDirectionX() {
-  if (piedra.x > boxRight) {
-    piedra.direccionX = "izquierda";
-  } else if (piedra.x < -boxRight) {
-    piedra.direccionX = "derecha";
-  }
-}
+const nepiedra = new element("tijera.png", "tijera");
+console.log(nepiedra.getDirectionX());
+caja.appendChild(nepiedra.node)
 
-
-
-function movement() {
-  
-  getDirectionY();
-  getDirectionX();  
-  if (piedra.direccionX === "derecha") {
-    moveFoward();
-  } else {
-    moveBackward();
-  }
-  if (piedra.direccionY === "arriba") {
-    moveUp();
-  } else {
-    moveDown();
-  }
-}
-
+//start button
 boton.addEventListener("click", () => {
-  setInterval(movement, 100);
+  console.log(nepiedra.x)
+  console.log(nepiedra.y)
+  setInterval(nepiedra.movement(), 25);
 });
-
