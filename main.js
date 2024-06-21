20945d957ed945423a867374fe44f732e7064fe0
unknown <maximoalexisflorianmatos@gmail.com>
2024-06-20 21:20:24 -0400

    position bug fixed

--- main.js
+++ main.js
@@ -11,22 +11,18 @@ let boxRight = window.outerWidth - caja.offsetWidth;
 class element {
   src;
   alt;
-  direccionX;
-  direccionY;
   width = `50px`;
   step = 5;
-  y;
-  x;
   node;
 
   constructor(src, alt) {
     this.src = src;
     this.alt = alt;
-    this.direccionX = 'derecha';
-    this.direccionY = 'abajo'
-    this.x = 0;
-    this.y  = 0;
-    this.createDomElement()
+    this.x = Math.floor(Math.random()*100);
+    this.y = Math.floor(Math.random()*100);
+    this.direccionX =Math.floor(Math.random()*100) > 50 ? "derecha" : "izquierda" 
+    this.direccionY = Math.floor(Math.random()*100) > 50 ? "arriba" : "abajo" ;
+    this.createDomElement();
   }
 
   createDomElement() {
@@ -35,11 +31,11 @@ class element {
     element1.style.bottom = "0px";
     element1.style.right = "0px";
     element1.style.left = "445px";
-    element1.style.width = this.width
-    element1.src = this.src
-    element1.alt = this.alt
-    element1.style.position = 'absolute'
-    return this.node = element1;
+    element1.style.width = this.width;
+    element1.src = this.src;
+    element1.alt = this.alt;
+    element1.style.position = "absolute";
+    return (this.node = element1);
   }
 
   moveFoward() {
@@ -50,7 +46,7 @@ class element {
   moveBackward() {
     return (this.x -= this.step);
   }
-  
+
   //move on Y up
   moveUp() {
     return (this.y -= this.step);
@@ -72,37 +68,42 @@ class element {
     if (this.x > boxRight) {
       this.direccionX = "izquierda";
     } else if (this.x < -boxRight) {
-      this.direccionX = "derecha";
+     this.direccionX = "derecha";
     }
   }
-  // make the element bounce 
+  // make the element bounce
   movement() {
     this.getDirectionX();
     this.getDirectionY();
     if (this.direccionX === "derecha" && this.direccionY === "arriba") {
-      this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveDown()}px)`;
-    } else if (
+      return this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveDown()}px)`;
+    } 
+    else if (
       this.direccionX === "izquierda" &&
       this.direccionY === "arriba"
-    ) {
-      this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveDown()}px)`;
-    } else if (this.direccionX === "derecha" && this.direccionY === "abajo") {
-      this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveUp()}px)`;
-    } else if (
-      this.direccionX === "izquierda" &&
-      this.direccionY === "abajo"
-    ) {
-      this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveUp()}px )`;
+    ) 
+    {
+      return this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveDown()}px)`;
+    } 
+    else if (this.direccionX === "derecha" && this.direccionY === "abajo") 
+      {
+      return this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveUp()}px)`;
+    } 
+    else if (this.direccionX === "izquierda" && this.direccionY === "abajo") 
+      {
+      return this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveUp()}px )`;
     }
-   }
+  }
 }
-// 
-const nepiedra = new element("tijera.png", "tijera");
-caja.appendChild(nepiedra.node)
+//
+const tijera = new element("tijera.png", "tijera");
+caja.appendChild(tijera.node);
+const piedra = new element("rock.png", "piedra");
+caja.appendChild(piedra.node);
+
 
 //start button
 boton.addEventListener("click", () => {
-  console.log(nepiedra.x)
-  console.log(nepiedra.y)
-  setInterval(nepiedra.movement(), 100);
+  setInterval(function () { tijera.movement(); },25)
+  setInterval(function () { piedra.movement(); },25)
 });
