1ba027a4b4268d85c92bddccc7c056f5627a102e
unknown <maximoalexisflorianmatos@gmail.com>
2024-06-26 16:21:30 -0400

    multiple insertions and change by crossing working

--- main.js
+++ main.js
@@ -9,27 +9,39 @@ let boxLeft = caja.offsetLeft;
 let boxRight = window.outerWidth - caja.offsetWidth;
 
 class element {
+  id;
   src;
   alt;
-  width = `50px`;
+  tipo = "piedra" || "papel" || "tijeras";
+  width = `25px`;
   step = 5;
   node;
 
-  constructor(src, alt) {
-    this.src = src;
-    this.alt = alt;
-    this.x =0 
-    // Math.floor(Math.random()*100);
-    this.y = 0
-    // Math.floor(Math.random()*100);
-    this.direccionX = 'derecha'
-    // Math.floor(Math.random()*100) > 50 ? "derecha" : "izquierda" 
-    this.direccionY = 'arriba'
-    // Math.floor(Math.random()*100) > 50 ? "arriba" : "abajo" ;
-    this.createDomElement();
+  constructor(tipo, caja, id) {
+    this.id = id;
+    this.elementChoices(tipo);
+    this.tipo = tipo;
+    this.alt = "elemt";
+    this.x = Math.floor(Math.random() * 100);
+    this.y = Math.floor(Math.random() * 100);
+    this.direccionX = Math.floor(Math.random() * 1000) > 500 ? "derecha" : "izquierda";
+    this.direccionY = Math.floor(Math.random() * 1000) > 500 ? "arriba" : "abajo";
+    this.createDomElement(caja);
   }
 
-  createDomElement() {
+  elementChoices(tipo) {
+    if (tipo === "piedra") {
+      return (this.src = "rock.png");
+    }
+    if (tipo === "tijera") {
+      return (this.src = "tijera.png");
+    }
+    if (tipo === "papel") {
+      return (this.src = "paper.png");
+    }
+  }
+
+  createDomElement(caja) {
     const element1 = document.createElement("img");
     element1.style.top = "250px";
     element1.style.bottom = "0px";
@@ -39,6 +51,7 @@ class element {
     element1.src = this.src;
     element1.alt = this.alt;
     element1.style.position = "absolute";
+    caja.appendChild(element1);
     return (this.node = element1);
   }
 
@@ -72,56 +85,74 @@ class element {
     if (this.x > boxRight) {
       this.direccionX = "izquierda";
     } else if (this.x < -boxRight) {
-     this.direccionX = "derecha";
+      this.direccionX = "derecha";
     }
   }
+
+  changeTheValue(element) {
+    for (let i = 0; i < element.length - 1; i++) {
+      if (element[i].offsetLeft === this.offsetLeft && element[i].offsetTop === this.offsetTop && element.id !== this.id) {
+        if (element[i].tipo === "piedra" && this.tipo === "tijera" ) {
+          this.node.src = element[i].src;
+          this.tipo = element[i].tipo;
+          break;
+        } else if (element[i].tipo === "tijera" && this.tipo === "papel") {
+          this.node.src = element[i].src;
+          this.tipo = element[i].tipo;
+          break;
+        } else if (element[i].tipo === "papel" && this.tipo === "piedra") {
+          this.node.src = element[i].src;
+          this.tipo = element[i].tipo;
+          break;
+        }
+      }
+    }
+  }
+
   // make the element bounce
-  movement() {
+  movement(element) {
     this.getDirectionX();
     this.getDirectionY();
     if (this.direccionX === "derecha" && this.direccionY === "arriba") {
-      return this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveDown()}px)`;
-    } 
-    else if (
+      this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveDown()}px)`;
+    } else if (
       this.direccionX === "izquierda" &&
       this.direccionY === "arriba"
-    ) 
-    {
-      return this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveDown()}px)`;
-    } 
-    else if (this.direccionX === "derecha" && this.direccionY === "abajo") 
-      {
-      return this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveUp()}px)`;
-    } 
-    else if (this.direccionX === "izquierda" && this.direccionY === "abajo") 
-      {
-      return this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveUp()}px )`;
+    ) {
+      this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveDown()}px)`;
+    } else if (this.direccionX === "derecha" && this.direccionY === "abajo") {
+      this.node.style.transform = `translate(${this.moveFoward()}px,${this.moveUp()}px)`;
+    } else if (this.direccionX === "izquierda" && this.direccionY === "abajo") {
+      this.node.style.transform = `translate(${this.moveBackward()}px,${this.moveUp()}px )`;
     }
+    this.changeTheValue(element);
   }
 }
 
 //Creating elements
-let tijera = new element("tijera.png", "tijera");
-
-let piedra = new element("rock.png", "piedra");
-
-let papel = new element("paper.png", "papel");
-caja.appendChild(tijera.node);
-  caja.appendChild(piedra.node);
-// convert trade
-function convert(){
-  if (piedra.x === tijera.x && piedra.y === tijera.y )
-    {
-      console.log('change')
-      return tijera.src = piedra.src 
-    }
-}
+const elements = [
+  new element("piedra", caja, 1),
+  new element("tijera", caja, 2),
+  new element("papel", caja, 3),
+  new element("piedra", caja,4),
+  new element("tijera", caja,5),
+  new element("papel", caja,6),
+  new element("piedra", caja,7),
+  new element("tijera", caja,8),
+  new element("papel", caja,9),
+  new element("piedra", caja,10),
+  new element("tijera", caja,11),
+  new element("papel", caja,12),
+  new element("piedra", caja,13),
+  new element("tijera", caja,14),
+  new element("papel", caja,15),
+];
 
 //start button
 boton.addEventListener("click", () => {
-  
-  setInterval(function () { convert();}, 25 )
-  setInterval(function () { tijera.movement(); },25)
-  setInterval(function () { piedra.movement(); },25)
-  setInterval(function () { papel.movement(); },25)
+  for (let i = 0; i < elements.length; i++) {
+    setInterval(function () {
+      elements[i].movement(elements);
+    }, 25);
+  }
 });
